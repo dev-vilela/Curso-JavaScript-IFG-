a163e45e1f5be5a988a8a776ea743a668eb52d49
dev-vilela <paulov4@hotmail.com>
2024-11-10 13:12:16 -0300

    AULA CONDIÇÕES E LAÇOS DE REPETIÇÃO

--- js/index.js
+++ js/index.js
@@ -5,10 +5,31 @@
 
 // alert(numeroA + " x " + numeroB + " = " + resultado)
 
-let raio = prompt("Digite o raio: ");
-//AREA = PI * R^2
+// let raio = prompt("Digite o raio: ");
+// //AREA = PI * R^2
 
-const pi = 3.14;
-let area = pi * Math.pow(raio,2);
+// const pi = 3.14;
+// let area = pi * Math.pow(raio,2);
 
-alert("A area de um circulo com r= " + raio + " é " + area )
+// alert("A area de um circulo com r= " + raio + " é " + area )
+
+/*AULA CONDIÇÕES E LAÇOS DE REPETIÇÃO */
+
+let lista = [];
+let novoItem = prompt("Digite o nome do item ou - se não quiser adicionar");
+
+while(novoItem != "-"){
+    lista.push(novoItem)
+    novoItem = prompt("Digite o nome do item ou - se não quiser adicionar");
+}
+
+if(lista.length == 0){
+    alert("A lista esta vazia!")
+}else{
+    alert("Você deve comprar estes itens: ")
+    for(let i=0; i < lista.length; i++){
+        alert(lista[i]);
+    }
+}
+
+
