899a8f756fa8682bd751b786994263e90eae1565
dev-vilela <paulov4@hotmail.com>
2024-11-10 09:30:43 -0300

    Função para calcular um raio

--- js/index.js
+++ js/index.js
@@ -4,3 +4,11 @@
 // let resultado = numeroA * numeroB
 
 // alert(numeroA + " x " + numeroB + " = " + resultado)
+
+let raio = prompt("Digite o raio: ");
+//AREA = PI * R^2
+
+const pi = 3.14;
+let area = pi * Math.pow(raio,2);
+
+alert("A area de um circulo com r= " + raio + " é " + area )
