ab7de0892efb20180026e2a55c3d9422e53213f8
dev-vilela <paulov4@hotmail.com>
2024-11-10 09:23:20 -0300

    Calculo basico de multiplicação

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// let numeroA = prompt("Digite o primeiro numero: ")
+// let numeroB = prompt("Digite o segundo numero: ")
+
+// let resultado = numeroA * numeroB
+
+// alert(numeroA + " x " + numeroB + " = " + resultado)
